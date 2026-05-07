a6f1182374ccaf0c9abdf6345337d590c544f557
allanboas <allanboas.ot@gmail.com>
2026-05-06 21:00:12 -0300

    gestao-clientes

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id": 1, "nome": 'João', "email": 'joao@example.com'},
+    {"id": 2, "nome": 'Mariana', "email": 'mariana@example.com'},
+    {"id": 3, "nome": 'Catarina', "email": 'catarina@example.com'},
+    {"id": 4, "nome": 'Bernardo', "email": 'bernardo@example.com'},
+    {"id": 6, "nome": 'Sebastião', "email": 'sebastiao@example.com'},
+    {"id": 7, "nome": 'Pedro', "email": 'pedro@example.com'}
+]
